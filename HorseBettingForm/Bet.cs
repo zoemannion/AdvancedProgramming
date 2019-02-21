cdba4bf6fb77fc8ac199a35b8d1d95690a57435f
zoemannion <46499526+zoemannion@users.noreply.github.com>
2019-02-21 20:44:33 +0000

    Add files via upload

---
+++ HorseBettingForm/Bet.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace HorseBettingForm
+{
+    public class Bet
+    {
+
+        public string Course { get; set; }
+        
+        public DateTime Date { get; set; }
+        
+        public double Amount { get; set;}
+
+        public string IfWin { get; set; }
+
+    
+
+    public Bet()
+    {
+
+    }
+    public Bet(string course, DateTime date, double amount, string win)
+    {
+            Course = course;
+            Date = date;
+            Amount = amount;
+            IfWin = win;
+    }
+
+        private IEnumerable<Bet> ReadBets()
+        {
+            List<Bet> dataCollection = new List<Bet>();
+            StreamReader sr = new StreamReader(@"BetData.txt");
+            using (sr)
+            {
+                string line = string.Empty;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var data = line.Split(',');
+                    dataCollection.Add(new Bet(data[0], Convert.ToDateTime(data[1]), Convert.ToDouble(data[2]), data[3]));
+                }
+            }
+            return dataCollection;
+        }
+
+        public string MostPopularCourse()
+        {
+            IEnumerable<Bet> bets = ReadBets();
+            var mostPopularCourse = bets
+                                    .GroupBy(x => x.Course)
+                                    .Where(g => g.Count() >= 1)
+                                    .OrderByDescending(g => g.Count())
+                                    .Select(g => g.Key)
+                                    .FirstOrDefault();
+            return mostPopularCourse;
+        }
+
+        public List<string> BetsDateOrder()
+        {
+            string win;
+            IEnumerable<Bet> bets = ReadBets();
+                var betsDateOrder = bets.OrderByDescending(x => x.Date)
+                         .Select(x => String.Format("{0}, {1}, {2}, {3}", x.Course, string.Format("{0:MM/dd/yyyy}", x.Date), x.Amount, win = x.IfWin == "true"? "Win" : "Loss"))
+                         .ToList();
+                return betsDateOrder;
+        }
+
+        public string LargestWonLost()
+        {
+
+            IEnumerable<Bet> bets = ReadBets();
+            var LargestWon = bets
+                             .Where(w => w.IfWin == "true")
+                             .OrderByDescending(x => x.Amount)
+                             .Select(x => x.Amount)
+                             .FirstOrDefault();
+
+            var LargestLoss = bets
+                            .Where(w => w.IfWin == "false")
+                            .OrderByDescending(x => x.Amount)
+                            .Select(x => x.Amount)
+                            .FirstOrDefault();
+
+            return "Largest won: " + LargestWon + "m" + "\t" + "Largest loss: " + LargestLoss + "m";
+
+        }
+
+        public string Success()
+        {
+            IEnumerable<Bet> bets = ReadBets();
+            var TotalRaces = bets
+                            .Count();
+            var TotalWins = bets
+                            .Where(g => g.IfWin == "true")
+                            .Select(g => g.Course)
+                            .Count();
+
+            return "Total races: " + TotalRaces + "\t" + "Total wins: " + TotalWins;
+        }
+
+        public List<string> AmountsByYear()
+        {
+            IEnumerable<Bet> bets = ReadBets();
+
+            var AmountsByYear = bets
+                    .GroupBy(i => i.Date.Year)
+                    .Select(g => String.Format("Year: {0} \t Total Won: {1} Total Lost: {2}", g.Key, g.Sum(i => i.IfWin == "true" ? i.Amount : 0), g.Sum(i => i.IfWin == "false" ? i.Amount : 0)
+                    )).ToList();
+            return AmountsByYear;
+        }
+
+
+
+    }
+
+}
---
+++ HorseBettingForm/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HorseBettingForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                Submit();
+            }
+        }
+
+
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+
+
+
+        private void btnPopularCourse_Click(object sender, EventArgs e)
+        {
+            lstMostPopularCourse.Items.Clear();
+            Bet b = new Bet();
+            lstMostPopularCourse.Items.Add(b.MostPopularCourse());
+        }
+
+        private void btnBetsDateOrder_Click(object sender, EventArgs e)
+        {
+            Bet b = new Bet();         
+            lstBetsByDate.DataSource = b.BetsDateOrder();
+        }
+
+        private void btnLargestWonLost_Click(object sender, EventArgs e)
+        {
+            lstLargestWinLoss.Items.Clear();
+            Bet b = new Bet();
+            lstLargestWinLoss.Items.Add(b.LargestWonLost());
+        }
+
+        private void btnSuccess_Click(object sender, EventArgs e)
+        {
+            lstSuccess.Items.Clear();
+            Bet b = new Bet();
+            lstSuccess.Items.Add(b.Success());
+        }
+
+        private void btnAmountsByYear_Click(object sender, EventArgs e)
+        {
+            Bet b = new Bet();
+            lstAmountsByYear.DataSource = b.AmountsByYear();
+        }
+
+       
+
+        private void txtAmount_TextChanged(object sender, EventArgs e)
+        {
+                if (System.Text.RegularExpressions.Regex.IsMatch(txtAmount.Text, @"[^\d*\.?\d*$]"))
+                {
+                    MessageBox.Show("Please enter numbers");
+                    txtAmount.Text = txtAmount.Text.Remove(txtAmount.Text.Length - 1); 
+                }
+        }
+
+        private void ClearForm()
+        {
+            txtRaceCourse.Clear();
+            txtAmount.Clear();
+        }
+
+        private void Submit()
+        {
+            if(txtRaceCourse.Text.Length > 1)
+            {
+                txtRaceCourse.Text = txtRaceCourse.Text.Substring(0, 1).ToUpper() + txtRaceCourse.Text.Substring(1).ToLower();
+            }
+            else
+            {
+                MessageBox.Show("Please enter correct course information");
+            }
+
+
+            string course = txtRaceCourse.Text;
+            string date = dtpDate.Text;
+            string amount = txtAmount.Text;
+            string winloss = cboWinorLoss.Text;
+
+            if (course.Length == 0 || amount.Length == 0 || winloss.Length == 0)
+            {
+                MessageBox.Show("Please enter all information");
+            }
+            else if (DateTime.Parse(date) > DateTime.Now)
+            {
+                MessageBox.Show("Date must be in the past");
+            }
+            else
+            {
+                using (var sw = new StreamWriter(@"BetData.txt", true))  // True to append data to the file; false to overwrite the file
+                {
+                    if (winloss == "Win")
+                    {
+                        string ifwin = "true";
+                        sw.WriteLine(string.Format("{0},{1},{2},{3}", new object[] { course, date, amount, ifwin }));
+                    }
+                    else
+                    {
+                        string ifwin = "false";
+                        sw.WriteLine(string.Format("{0},{1},{2},{3}", new object[] { course, date, amount, ifwin }));
+                    }
+                }
+                MessageBox.Show("Submitted");
+                ClearForm();
+            }
+        }
+
+    }
+}
+
+
+    
+
+
+
+
